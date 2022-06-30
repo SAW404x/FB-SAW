8f69f3ec798d28316dcbef7b527820163e32583d
SAW <97469994+SAW404x@users.noreply.github.com>
2022-06-30 12:15:10 +0300

    Delete SAW.py

--- SAW.py
+++
@@ -1,6 +0,0 @@
-import platform
-import os, platform
-os.system("cd $HOME/")
-#os.system("termux-setup-storage")
-os.system("xdg-open https://instagram.com/jb1lr?igshid=YmMyMTA2M2Y=")
-b = platform.architecture()[0]
